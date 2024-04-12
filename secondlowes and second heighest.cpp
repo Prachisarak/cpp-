0a2eeead59934474bf843d4842caacdd8e00b90a
prachisarak <sarakprachi@gmail.com>
2024-04-12 16:13:17 +0530

    second high and low

---
+++ secondlowes and second heighest.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n,i;
+	int arr[n];
+	cout<<"enter array size";
+	cin>>n;
+	
+	for(i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	for(i=0;i<n;i++){
+		cout<<arr[i]<<" ";
+	}
+	
+}
